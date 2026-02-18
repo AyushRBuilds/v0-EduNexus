ef93d6fcd5fbf42ae5eb3c245b66937436dbca68
Ayush Redekar <ayushr.builds@gmail.com>
2026-02-18 21:39:17 +0530

    Deleted some files

--- edunexus-backend-main/edunexus-backend-main/src/main/java/com/edunexus/backend/repository/SubjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edunexus.backend.repository;
-
-import com.edunexus.backend.entity.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-    List<Subject> findByDepartmentAndSemester(String department, Integer semester);
-}
--- edunexus-backend-main/edunexus-backend-main/src/main/java/com/edunexus/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edunexus.backend.repository;
-
-import com.edunexus.backend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
